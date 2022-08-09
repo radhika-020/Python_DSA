1daf27e907c1068035d4cd31f11a77db8ceae9f0
Radhika Arora <aradhika880@gmail.com>
2022-08-09 15:12:46 +0530

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,8 @@
+def selection_sort(arr):
+  n = len(arr)
+  for i in range(0, n-1):
+    min = i
+    for j in range(i + 1, n):
+      if arr[min] > arr[j]:
+        min = j
+     arr[i], arr[min] = arr[min], arr[i]
